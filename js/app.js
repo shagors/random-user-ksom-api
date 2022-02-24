4577ef7534448d0c45bc23f3a87936376e5f394d
shagors <shajjadshagor@gmail.com>
2022-02-24 15:43:39 +0600

    user address and location add

--- js/app.js
+++ js/app.js
@@ -1,1 +1,24 @@
-console.log('i am ok');
+const loadBuddies = () => {
+    fetch('https://randomuser.me/api/?results=500')
+        .then(res => res.json())
+        .then(data => displayBuddies(data));
+}
+loadBuddies();
+
+const displayBuddies = data => {
+    const buddies = data.results;
+    const buddiesDiv = document.getElementById('random-user');
+    for(const buddy of buddies){
+        console.log(buddy);
+        const p = document.createElement('p');
+        p.classList.add('user');
+        p.innerText = `
+        Name: ${buddy.name.first}
+        Email: ${buddy.email},Phone: ${buddy.cell}
+        Gender: ${buddy.gender}
+        Birth-date: ${buddy.dob.date}
+        Zone: ${buddy.location.city},${buddy.location.timezone.offset},Latitude: ${buddy.location.coordinates.latitude},State: ${buddy.location.state}, Country: ${buddy.location.country}
+        `;
+        buddiesDiv.appendChild(p);
+    }
+}
